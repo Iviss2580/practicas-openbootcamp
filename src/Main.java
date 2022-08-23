fcaa6cbc1161155ec0a4a9235718ed84fd221a71
Iviss2580 <iviss036@hotmail.com>
2022-08-23 13:00:22 -0300

    ejercicio 3

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Coche miCoche = new Coche();
+        miCoche.SumarPuertas();
+        miCoche.SumarPuertas();
+        System.out.println(miCoche.puertas);
+    }
+
+
+    static class Coche {
+        public int puertas = 4;
+        public void SumarPuertas(){
+            this.puertas++;
+        }
+    }
+}
